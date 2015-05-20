2fe9cd0115d45c5af84fc924f8ce8f36acf45343
Ryan Hayle <rhayle@arcstone.com>
2015-05-20 18:11:15 -0500

    Added custom resolution methods to @Bind
    If the "uses" parameter is present, the scanner will generate a
    Route::bind instead of Route::model.  The resolution method is specified
    in "action" format: "FQCN@method".  If a method isn't specified, the
    default "bind" method is called on the specified class.
    E.g.: @Bind("user", uses="App\User@findBySlug")

--- src/Database/Eloquent/Annotations/Annotations/Bind.php
+++ src/Database/Eloquent/Annotations/Annotations/Bind.php
@@ -10,9 +10,16 @@ class Bind
     /**
      * The binding the annotation binds the model to.
      *
-     * @var array
+     * @var string
      */
     public $binding;
+
+    /**
+     * Optional class (and method) used to resolve the model instance.
+     *
+     * @var string
+     */
+    public $binder = null;
 
     /**
      * Create a new annotation instance.
@@ -24,5 +31,8 @@ class Bind
     public function __construct(array $values = [])
     {
         $this->binding = $values['value'];
+        if (isset($values['uses'])) {
+            $this->binder = $values['uses'];
+        }
     }
 }
